562dd6173274f9cc24ff2f86cd76ead8aaa9ddea
AnatoliyProhortsev <tolik.spb.ru@yandex.ru>
2023-01-15 23:34:49 +0300

    Нарисована главная форма

---
+++ Fly_Model/Fly_Model.cpp
@@ -0,0 +1,15 @@
+#include "Fly_Model.h"
+#include "ui_Fly_Model.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
